dfcb53a3463befcfdce624c0d73ec65b0a8e6ab7
prateekpandey <prateekpandey9928@gmail.com>
2024-07-23 18:46:39 +0530

    23 july

---
+++ LC Daily/Python solutions/Sequential digits.py
@@ -0,0 +1,17 @@
+# 1291. Sequential Digits
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        result, mn, mx = [], 10, 1000000000
+        for i in range(1,10):
+            num = 0
+            for digit in range(i,10):
+                num = num*10 + digit
+                if num < mn:
+                    continue
+                if num > mx:
+                    break
+                if num <= high and num >= low:
+                    result.append(num)
+        
+        return sorted(result)
